8b4789575234acd32594482ab9d016d3a7cc3092
AnhPha <phatran1704@gmail.com>
2025-11-02 02:19:12 +0700

    teacherList

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,31 +1,49 @@
-import List from "@/components/list";
 import PageNumber from "@/components/pageNumber";
 import Table from "@/components/table";
 import TableSearch from "@/components/tableSearch";
+import { role, teachersData } from "@/lib/data";
 import { headers } from "next/headers";
 import Image from "next/image";
+import Link from "next/link";
+
+type Teacher = {
+  id: number;
+  teacherId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone: string;
+  subjects: string[];
+  classes: string[];
+  address: string;
+};
 
 const columns = [
   { header: "Thông tin", accessor: "info" },
   {
     header: "ID giáo viên",
     accessor: "teacherId",
-    className: "hidden md: table-cell",
+    className: "hidden md:table-cell",
   },
   {
     header: "Môn dạy",
     accessor: "subjects",
-    className: "hidden md: table-cell",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Lớp",
+    accessor: "classes",
+    className: "hidden md:table-cell",
   },
   {
     header: "SDT",
     accessor: "phone",
-    className: "hidden lg: table-cell",
+    className: "hidden lg:table-cell",
   },
   {
     header: "Địa chỉ",
     accessor: "address",
-    className: "hidden lg: table-cell",
+    className: "hidden lg:table-cell",
   },
   {
     header: "Liên hệ",
@@ -34,6 +52,46 @@ const columns = [
 ];
 
 const TeacherListPage = () => {
+  const renderRow = (item: Teacher) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-teal-50"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <Image
+          src={item.photo}
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-[var(--color-boyC)]">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-[var(--color-pink)]">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
     <div className="bg-white p-4 rounded-2xl flex-1 m-4 mt-0">
       {/* TOP */}
@@ -55,7 +113,7 @@ const TeacherListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} />
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
       {/* PAGENUMBER */}
       <PageNumber />
     </div>
